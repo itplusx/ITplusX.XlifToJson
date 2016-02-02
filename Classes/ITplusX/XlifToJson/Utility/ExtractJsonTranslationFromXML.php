ae3783f60318482f3e975c0e9c4dfca7e2539921
sdukart <dukart@itplusx.de>
2016-02-02 15:24:54 +0100

    [FEATURE] Add class to extract the relevant information from xml into the desired json format.

---
+++ Classes/ITplusX/XlifToJson/Utility/ExtractJsonTranslationFromXML.php
@@ -0,0 +1,35 @@
+<?php
+namespace ITplusX\XlifToJson\Utility;
+
+class ExtractJsonTranslationFromXML
+{
+    /**
+     * Extracts relevant translations from translation XML
+     *
+     * @param SimpleXML $xml Translation as XML format
+     *
+     * @return string
+     */
+    public static function extractJsonFromXML($xml)
+    {
+        $entriesWithJsonAttr = $xml->xpath("//trans-unit[@json]");
+
+        $jsonString = "{\n";
+        foreach ($entriesWithJsonAttr as $entry => $data) {
+            if ($entry != 0) {
+                $jsonString .= ",\n";
+            }
+            $attributes = $data->attributes();
+
+            $jsonId = $attributes['json'];
+            $source = $data->xpath('source');
+            $target = $data->xpath('target');
+
+            $jsonString .= '    "' . $jsonId . '": "' . (count($target) == 1 ? $target{0} : $source{0}) . '"';
+        }
+
+        $jsonString .= "\n}";
+
+        return $jsonString;
+    }
+}
