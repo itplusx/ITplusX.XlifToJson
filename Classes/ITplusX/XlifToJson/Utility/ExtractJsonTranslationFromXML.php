d2647c0101aad06bcc3ecb28b49c98d763c33a98
sdukart <dukart@itplusx.de>
2016-02-02 16:20:06 +0100

    [FEATURE] Trim and remove \n from translations Also encoding in UTF8 now.

--- Classes/ITplusX/XlifToJson/Utility/ExtractJsonTranslationFromXML.php
+++ Classes/ITplusX/XlifToJson/Utility/ExtractJsonTranslationFromXML.php
@@ -6,8 +6,8 @@ class ExtractJsonTranslationFromXML
     /**
      * Extracts relevant translations from translation XML
      *
-     * @param SimpleXML $xml Translation in XML format
-     * @param string $attr Name of the attribute indicating it's relevance
+     * @param SimpleXML $xml  Translation in XML format
+     * @param string    $attr Name of the attribute indicating it's relevance
      *
      * @return string
      */
@@ -19,13 +19,15 @@ class ExtractJsonTranslationFromXML
         foreach ($entriesWithJsonAttr as $entry => $data) {
             $attributes = $data->attributes();
 
-            $jsonId = $attributes[$attr];
+            $jsonId = (string)$attributes[$attr];
             $source = $data->xpath('source');
             $target = $data->xpath('target');
 
-            $json[(string)$jsonId] = count($target) == 1 ? (string)$target{0} : (string)$source{0};
+            $json[(string)$jsonId] = (count($target) == 1)
+                ? trim(preg_replace("/\\n/", '', (string)$target{0}))
+                : trim(preg_replace("/\\n/", '', (string)$source{0}));
         }
 
-        return json_encode($json);
+        return json_encode($json, JSON_UNESCAPED_UNICODE);
     }
 }
